c0860113aab2833ce504cf6655501cecb57172dd
Kevin Wei <kevin.w.4280@gmail.com>
2025-04-16 17:13:08 -0700

    single
    single blog formatting

--- djangoblog/blog/models.py
+++ djangoblog/blog/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class BlogPost(models.Model):
-    subject = models.CharField(max_length=200)
+    topic = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
@@ -11,4 +12,4 @@ class BlogPost(models.Model):
     image = models.ImageField(default = 'fallback.png', blank = True)
 
     def __str__(self):
-        return self.description
+        return self.title
